91f613041a14f3af976ab249aac411f7b57ede14
Policepigeon <policepigeon01@gmail.com>
2024-10-08 14:14:43 +0100

    social

---
+++ AntiSocialSocials/ASS.cpp
@@ -0,0 +1,26 @@
+/*the anti social social*/
+using namespace std;
+
+#include <iostream>
+
+    class user{
+        public:
+        string name;
+        int uid;
+        string email;
+        string password;
+
+    };
+
+int main(){
+    user u1;
+    cout << "Enter the name: ";
+    cin >> u1.name;
+    random.randint(1,100) >> u1.name;
+    cout << "Enter the email: ";
+    cin >> u1.email;
+    cout << "Enter password";
+    cin >> u1.password;
+
+
+return 0;}
---
+++ tests/IfElse.cpp
@@ -0,0 +1,11 @@
+/*this is for if else loops*/
+using namespace std;
+#include <iostream>
+
+int main() {
+    int x = 100;
+    if (x < 10 ){
+        cout << "x is less than 10";
+        return 0;
+    }
+}
