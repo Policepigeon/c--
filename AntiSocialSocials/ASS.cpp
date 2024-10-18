e8fedfcf8f15c9ff24b16043b6306f35cedde4f6
Policepigeon <policepigeon01@gmail.com>
2024-10-18 11:27:59 +0100

    antisocial social was done in bio

--- AntiSocialSocials/ASS.cpp
+++ AntiSocialSocials/ASS.cpp
@@ -3,27 +3,55 @@ using namespace std;
 
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
     class user{
-        public:
+        //attributes, both private and public
+        public:  //dont set the password and the email to public
         string name;
         int uid;
+        private:
         string email;
         string password;
+        
+        //making a setter for email and password
+        public:
+        void setemail (string emailtemp){
+            email = emailtemp;
+        }
+        void setpassword (string passwordtemp){
+            password = passwordtemp;
+        }
+        string getEmail() {
+            return email;
+        }
+        
+        string getPassword() {
+            return password;
+        }
 
     };
-
 int main(){
-    int nextuid = 0
+    int nextuid = 0;
+    string emailtemp;
+    string passwordtemp; 
     user u1;
-    nextuid >> u1.uid;
-    nextuid + 1 >> nextuid
-    cout << "Enter the email: ";
-    cin >> u1.email;
+    u1.uid = nextuid; //assigning the uid
+    nextuid++; //this increments the uid 
+    cout << "Enter the email: "; //getting the user's inputs for  the differing fields
+    cin >> emailtemp;
+    u1.setemail(emailtemp);
     cout << "Enter password";
-    cin >> u1.password;
+    cin >> passwordtemp;
+    u1.setpassword(passwordtemp);
     cout << "Enter a username";
     cin >> u1.name;
 
+    //now i can return the values from the class user to check that they worked
+    cout<< "username is " << u1.name << endl;
+    cout << "uid is " << u1.uid << endl;
+    cout<< "email is " << u1.getEmail() << endl;
+    cout << "password is " << u1.getPassword() << endl;
 
-return 0;}
+    return 0;
+}
