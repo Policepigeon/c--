4ad5685c585ca75c5b00921e870aaccc7c8dfdee
Policepigeon <policepigeon01@gmail.com>
2024-11-06 10:16:50 +0000

    openday rush

---
+++ tests/OpenDay.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+int peopleclicked = 10;
+return 0;
+
+
+switch (peopleclicked){
+  case 10:
+  cout << "you are the tenth person";
+  case 100:
+  cout << "you are the hundredth person";
+return 0;}
+cout << "You have clicked on the button "<< peopleclicked <<" times.";
+}
