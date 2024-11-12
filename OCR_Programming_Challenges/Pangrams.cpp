643df85ee0573a4920333d49b2c42ad58656213a
Policepigeon <policepigeon01@gmail.com>
2024-11-12 13:04:56 +0000

    start work on pangrams

---
+++ OCR_Programming_Challenges/Pangrams.cpp
@@ -0,0 +1,13 @@
+/* this is a piece of code that finds pangrams in a user input*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+string UserWord;
+cout << "type the word you want to check for pangrams \n";
+
+  return 0;
+}
