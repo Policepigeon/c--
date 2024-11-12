dab5f4d3b10f89096fb6372bc685ebdbb29f66e5
Policepigeon <policepigeon01@gmail.com>
2024-11-12 14:27:25 +0000

    evil floating point bit level hacking

---
+++ FastInverseSquareRootButBetter.cpp
@@ -0,0 +1,16 @@
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;						// evil floating point bit level hacking
+	i  = 0x5FE6EB50C7B537A9 - ( i >> 1 );               // what the fuck?         but better
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
