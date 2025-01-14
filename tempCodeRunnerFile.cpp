b6241b21d3ce4ba94c3c22c237ba82af68972216
Policepigeon <policepigeon01@gmail.com>
2025-01-14 13:52:15 +0000

    jasper

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+d showCompletionScreen() {
+    std::cout << "\nCongratulations! You've completed all the puzzles!\n";
+    std::cout << "        *\n";
+    std::cout << "       /|\\\n";
+    std::cout << "      / | \\\n";
+    std::cout << "     /  |  \\\n";
+    std::cout << "    /   |   \\\n";
+    std::cout << "   /    |    \\\n";
+    std::cout << "  /     |     \\\n";
+    std::cout << " /______|______\\\n";
